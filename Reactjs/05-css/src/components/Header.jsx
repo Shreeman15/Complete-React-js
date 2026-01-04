659014120d9967c249d8b8688ad5526615c717c2
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-04 23:39:49 +0530

    dis advantage of css due to conflict

---
+++ Reactjs/05-css/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Google</h1>
+      <button className='btn'>login</button>
+    </div>
+  )
+}
+
+export default Header
