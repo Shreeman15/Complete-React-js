57fe5a73572e165220f04426b1d69f54aaf05812
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-17 12:37:39 +0530

    concept of batch updates in react

--- Reactjs/09-useState/src/App.jsx
+++ Reactjs/09-useState/src/App.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [num, setnum] = useState({userName : "shreeman" , userAge : 21})
-  
+  const [Number, setNumber] = useState([12,11,12,13])
+  const [counter, setcounter] = useState(0)
   // for changing users name and data function
   const changeUserData = () => {
     const newNum = {...num}
@@ -10,11 +11,29 @@ const App = () => {
      newNum.userAge = 22
      setnum(newNum)
   }
+  const ChangeNumber = () => {
+    const NewNumber = [...Number,22]
+    //NewNumber.push(11)
+    setNumber(NewNumber)
+  }
+
+   const increaseNumber = () => {
+    // concept of batch update in react
+      setcounter(prev => prev + 1)
+      setcounter(prev => prev + 1)
+      setcounter(prev => prev + 1)
+   }
   
   return (
     <div>
       <h1>{num.userName} , {num.userAge}</h1>
       <button onClick={changeUserData}>Click</button>
+
+      <h1>{Number}</h1>
+      <button onClick={ChangeNumber}>Change</button>
+
+      <h1>{counter}</h1>
+      <button onClick={increaseNumber}>by 3</button>
     </div>
   )
 }
