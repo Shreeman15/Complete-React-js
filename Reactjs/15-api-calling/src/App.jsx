023808567f74d09070ba533b3c91931b16080b45
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-18 23:43:00 +0530

    fetch method

--- Reactjs/15-api-calling/src/App.jsx
+++ Reactjs/15-api-calling/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+// api call 
+// 2 types
+// 1) fetch method
+// 2) through axios - 3rd party calling
+
+const fetchData = async() => {
+  console.log("fatched data");
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1') // async things so put async and await
+  const data = await response.json();
+  console.log(data);
+  console.log(data.id);
+  console.log(data.title);
+}
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <button onClick={fetchData}>button</button>
+    </div>
   )
 }
 
