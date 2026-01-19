ca53543bfc7e107b218ec9fd2a7b7358309ba829
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-19 14:07:00 +0530

    useEffect, things where run ||rly running with the main things

--- Reactjs/16-useeffect/src/App.jsx
+++ Reactjs/16-useeffect/src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+// useEffect 
 const App = () => {
+
+  const [first, setfirst] = useState(0)
+  const [second, setsecond] = useState(0)
+  
+  useEffect(() => {
+    console.log("from this first : "+first);
+    
+  },[first])
+
+  useEffect(() => {
+    console.log("from the second : "+second);
+  },[second])
+
   return (
-    <div>App</div>
+    <div className='w-full min-h-screen p-10 bg-gray-900 flex flex-col content-between gap-4 flex-wrap'>
+      <h1 className='text-white'>{first}</h1>
+      <h1 className='text-white'>{second}</h1>
+      <button onClick={() => {
+        setfirst(prev => prev + 1)
+      }}
+       className='bg-gray-800 px-8 py-4 rounded cursor-pointer active:scale-95 text-white hover:bg-gray-700'>
+        Click first
+        </button>
+      
+      <button onClick={() => {
+        setsecond(prev => prev + 1)
+      }} 
+      className='bg-gray-800 px-8 py-4 rounded cursor-pointer active:scale-95 text-white hover:bg-gray-700'>
+        Click second
+        </button>
+    </div>
   )
 }
 
