bef9133f45fe5d1b2565ac68f1283d498fc1a57e
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-22 21:50:25 +0530

    routers having reloading phase without using the Link form the react-router-dom

--- Reactjs/18-react-router-dom/src/App.jsx
+++ Reactjs/18-react-router-dom/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
+import Contact from './Pages/Contact.jsx'
+import Navbar from './Components/Navbar.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path = '/' element = {<Home/>} />
+        <Route path = '/About' element = {<About/>} />
+        <Route path = '/Contact' element = {<Contact/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ Reactjs/18-react-router-dom/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Nav'>
+      <h3>Shreeman</h3>
+      <div>
+        <a href="/ ">Home</a>
+        <a href="/About">About</a>
+        <a href="/Contact">Contact</a>
+      </div>
+  </div>
+  )
+}
+
+export default Navbar
